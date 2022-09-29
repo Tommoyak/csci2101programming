9932442b2331529e303734bac18ad0bd70bb767c
tommoyak <plumme15@go.stockton.edugit config --list git config --global user.email plumme15@go.stockton.edu>
2022-09-29 14:00:41 -0400

    Change date to Friday

---
+++ Week1/HelloWorld.java
@@ -0,0 +1,31 @@
+package Week1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I dont't like sports!"); // this is a string
+        System.out.println("3");
+        System.out.println('a');
+
+        System.out.println("Today is Friday 9/30/2022");
+
+        System.out.println('3');
+        // System.out.println(3+3); //Integer
+        // TYPE name = value;
+        int answer = 3 + 3;
+        System.out.println(answer);
+        String name = "Cammesa";
+
+        System.out.println(name + " said, Greg Watsh is a great teacher! 6GOD X420");
+        // String Concatenation
+        System.out.println();
+        System.out.println(answer + " " + name);
+        System.out.print("Hello\n");
+        System.out.println("I like green eggs and ham");
+
+        name = "Cammesa";
+
+        System.out.println(name);
+
+    }
+}
